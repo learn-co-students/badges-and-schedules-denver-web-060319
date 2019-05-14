a261d9c9644370e58a9d5727149a869eb2183436
“Patrick <pctennislessons@gmail.com>
2019-05-13 22:41:39 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badages = []
+    array.each do |x|
+     puts badages << "Hello, my name is #{x}."
+   end
+   badages
+ end
+
+def assign_rooms(array)
+    badages = []
+    array.each do |x|
+        puts badages << "Hello, #{x}! You'll be assigned to room #{array.index(x) + 1}!"
+    end
+    badages
+end
+
+def printer(array)
+   badges = batch_badge_creator(array)
+    badges.each do |x| 
+        puts x
+    end
+    rooms = assign_rooms(array)
+    rooms.each do |x|
+        puts x
+    end
+end
