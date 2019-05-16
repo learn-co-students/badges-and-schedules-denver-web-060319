aeec48ff088ffdec4ac2a0ca20d4e676545570ef
“Patrick <pctennislessons@gmail.com>
2019-05-15 20:27:58 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,27 +4,23 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-    badages = []
-    array.each do |x|
-     puts badages << "Hello, my name is #{x}."
+    array.map do |x|
+        "Hello, my name is #{x}."
     end
-    return badages
  end
 
 def assign_rooms(array)
-    rooms = []
-    array.each do |x|
-        puts rooms << "Hello, #{x}! You'll be assigned to room #{array.index(x) + 1}!"
+    array.each_with_index.map do |x, y|
+        "Hello, #{x}! You'll be assigned to room #{y + 1}!"
     end
-    return rooms
 end
+
 
 def printer(array)
     batch_badge_creator(array).each do |key|
         puts key
     end
-
-    assign_rooms(array).each do |key|
-        puts key
+    assign_rooms(array).each do |value|
+        puts value
     end
 end    
